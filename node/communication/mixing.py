53b4ead7810f2211fb5e36a6458f7f01374491ae
Sandrin Hunkeler <sandrin@hunkeler.ws>
2025-07-06 12:06:28 +0000

    cleanup

--- node/communication/mixing.py
+++ node/communication/mixing.py
@@ -2,42 +2,44 @@ import math
 import secrets
 import asyncio
 import logging
+
+from utils.logging_config import log_header
+from utils.config_store import ConfigStore
 from utils.exception_decorator import log_exceptions
 from metrics.node_metrics import metrics, MetricField
 from dataclasses import dataclass
 from typing import Awaitable, Callable
+
 
 @dataclass
 class QueueObject:
     send_message: Awaitable
     update_metrics: Callable
 
+
 class Mixer:
-    def __init__(self, enabled, mix_lambda, shuffle):
+    def __init__(self, node_config: ConfigStore):
         self._outbox = []
         self._cover_generator = None
         self._outbox_loop = None
-        self._config = {
-            "enabled": enabled,
-            "lambda": mix_lambda, 
-            "shuffle": shuffle,
-            "nr_cover_bytes": 100,
-        }
+        self._config = node_config
 
     # inverse transform sampling of exponential distribution
     @staticmethod
     def secure_exponential(q):
-        u = int.from_bytes(secrets.token_bytes(7), "big") / 2**56
+        u = int.from_bytes(secrets.token_bytes(7), "big") / 2 ** 56
         if q == 0:
             q = 0.0001
-        return -math.log(1 - u) / (1/q)
-    
+        sleep_time = -math.log(1 - u) / (1 / q)
+        # logging.warning(f"Secure exponential sleep time: {sleep_time} seconds, max_rate = {1 / sleep_time}")
+        return max(sleep_time, 1)
+
     @log_exceptions
     async def __outbox_loop(self):
-        while (True):
+        while True:
             interval = 0
-            if self._config["enabled"]:
-                interval = Mixer.secure_exponential(self._config["lambda"])
+            if self._config.mix_enabled:
+                interval = Mixer.secure_exponential(self._config.mix_lambda)
             await asyncio.sleep(interval)
             metrics().set(MetricField.OUT_INTERVAL, interval)
             if not self.queue_is_empty():
@@ -45,9 +47,9 @@ class Mixer:
                 await queue_obj.send_message()
                 queue_obj.update_metrics()
                 self.__udpdate_message_metric(sending_covers=False)
-            elif self.queue_is_empty() and self._config["enabled"]:
+            elif self.queue_is_empty() and self._config.mix_enabled:
                 if self._cover_generator != None:
-                    await self._cover_generator(nr_bytes=self._config["nr_cover_bytes"])
+                    await self._cover_generator(nr_bytes=self._config.nr_cover_bytes)
                     self.__udpdate_message_metric(sending_covers=True)
                 else:
                     logging.warning("No cover generator specified in mixer")
@@ -55,8 +57,11 @@ class Mixer:
     async def start(self, cover_generator: Callable):
         self._outbox_loop = asyncio.create_task(self.__outbox_loop())
         self._cover_generator = cover_generator
-        logging.info(f"Started mixer, enabled: {self._config['enabled']}, lambda: {self._config['lambda']}, shuffle: {self._config['shuffle']}")
-
+        log_header("Peer-Based Mixer")
+        logging.info(f"Enabled: {self._config.mix_enabled}")
+        logging.info(f"Lambda: {self._config.mix_lambda}")
+        logging.info(f"Shuffle: {self._config.mix_shuffle}")
+        logging.info(f"N Cover Bytes: {self._config.nr_cover_bytes}")
 
     def __shuffle_outbox(self):
         for i in reversed(range(1, len(self._outbox))):
@@ -64,18 +69,18 @@ class Mixer:
             self._outbox[i], self._outbox[j] = self._outbox[j], self._outbox[i]
         return self._outbox
 
-    def push_to_outbox(self, msg_coroutine : Awaitable, update_metrics: Callable):
+    def push_to_outbox(self, msg_coroutine: Awaitable, update_metrics: Callable):
         queue_obj = QueueObject(msg_coroutine, update_metrics)
         self._outbox.append(queue_obj)
 
-        if self._config["shuffle"]:
+        if self._config.mix_shuffle:
             self.__shuffle_outbox()
 
     def queue_is_empty(self):
         return len(self._outbox) == 0
 
     def __udpdate_message_metric(self, sending_covers):
-        if (sending_covers):
+        if sending_covers:
             metrics().increment(MetricField.COVERS_SENT)
         metrics().set(MetricField.SENDING_COVERS, 1 if sending_covers else 0)
         metrics().set(MetricField.SENDING_MESSAGES, 0 if sending_covers else 1)
