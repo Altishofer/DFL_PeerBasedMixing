54485858fd7848942479dcea0df1b91491cbc95c
ringdinglinn <lalaponii@gmail.com>
2025-07-21 14:54:13 +0000

    updated rtt experiments

--- node/utils/config_store.py
+++ node/utils/config_store.py
@@ -3,7 +3,7 @@ from typing import List
 
 @dataclass
 class ConfigStore:
-    max_hops: int = 3
+    max_hops: int = 1
     resend_time: int = 40
     push_metric_interval: int = 1
     timeout_model_collection: int = 60
@@ -12,13 +12,13 @@ class ConfigStore:
     dirichlet_alpha: float = 10.0
     port: int = 8000
     node_id: int = 0
-    n_nodes: int = 5
+    n_nodes: int = 10
     n_rounds: int = 1
     exit_nodes: List[int] = field(default_factory=lambda:[])
     join_nodes: List[int] = field(default_factory=lambda:[])
     stream_mode: bool = False
     mix_enabled: bool = True
-    mix_lambda: float = 0.5
+    mix_lambda: float = 0.001
     mix_shuffle: bool = True
     nr_cover_bytes: int = 100
     n_fragments_per_model: int = 10000
